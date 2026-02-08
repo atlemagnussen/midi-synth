f8bb590a886fe01e2509bcd2803a11cef70d65c1
Atle Magnussen <atlemagnussen@gmail.com>
2026-02-08 21:53:52 +0100

    f

--- client/src/synth/midiPlayer.ts
+++ client/src/synth/midiPlayer.ts
@@ -1,3 +1,8 @@
+interface OscilatorAndGain {
+    osc: OscillatorNode,
+    gain: GainNode
+}
+
 export class MidiPlayer {
     
     /**
@@ -27,44 +32,55 @@ export class MidiPlayer {
         const note = ev.data[1]
         const velocity = ev.data[2]
 
-        //console.log("command", command)
+        //console.log(`command: ${command}, note: ${note}, velocity: ${velocity}`)
         switch (command) {
             case 155:
-                if (velocity > 0)
-                    this.noteOn(note, velocity)
-                else
-                    this.noteOff(note)
+                this.noteOn(note, velocity)
+                break;
             case 139:
                 this.noteOff(note)
+                break;
         }
     }
 
-    oscilators: Record<string, OscillatorNode> = {}
+    
+    oscilators = new Map<number, OscilatorAndGain>()
 
     noteOn(note: number, velocity: number) {
-        //console.log(`note: ${note}, velocity: ${velocity}`)
+        //console.log(`note ON: ${note}, velocity: ${velocity}`)
         const osc = this.audioContext.createOscillator()
 
-        this.oscilators[`${note}`] = osc
-
         const gain = this.audioContext.createGain()
-        gain.gain.value = 0.33
+        gain.gain.value = this.velocityToGain(velocity)
 
         osc.type = "sine"
         osc.frequency.value = this.midiToFrequency(note)
         osc.connect(gain)
         gain.connect(this.audioContext.destination)
         osc.start()
+        this.oscilators.set(note, {osc, gain})
     }
 
     noteOff(note: number) {
-        console.log(`note off: ${note}`)
+        //console.log(`note off: ${note}`)
+        const noteData = this.oscilators.get(note)
+        
+        if (!noteData)
+            return
+
+        //noteData.gain.gain.setTargetAtTime(0, this.audioContext.currentTime, 0.01)
+        noteData.gain.gain.setValueAtTime(noteData.gain.gain.value, this.audioContext.currentTime)
+        noteData.gain.gain.exponentialRampToValueAtTime(0.0001, this.audioContext.currentTime + 0.3)
+        //noteData.osc.stop()
+        
+        this.oscilators.delete(note)
     }
+
     base = 440
     midiToFrequency(midiNode: number) {
         return (this.base / 32) * ( 2 ** ((midiNode - 9 ) /12 ))
     }
-    // midiAccess.inputs.forEach(input => {
-    //     input.addEventListener("midimessage", handleInput)  
-    // })
+    velocityToGain(velocity: number) {
+        return velocity / 127
+    }
 }
