afff06441405977f669a24dfb6a74210de7f3b11
Atle Magnussen <atlemagnussen@gmail.com>
2026-02-08 15:01:27 +0100

    d

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,22 +1,42 @@
-import "@ws/client/design/waLoader.js"
-import { html, LitElement } from "lit"
-import { customElement } from "lit/decorators.js"
+import {doImportWa} from "@ws/client/design/waLoader.js"
+import { html, LitElement, nothing } from "lit"
+import { customElement, state } from "lit/decorators.js"
 import { askPermission } from "./synth.js"
+
+doImportWa()
 
 @customElement("app-shell")
 export class AppShell extends LitElement {
 
+    @state()
+    midiDevicesConnected = 0
+
+    @state()
+    midiConnected = false
 
     async initSynth() {
         const access = await askPermission()
         console.log(access)
+
+        if (!access)
+            return
+
+        this.midiConnected = true
+        access.addEventListener("statechange", this.midiStateChange)
+    }
+
+    midiStateChange = (event: MIDIConnectionEvent) => {
+        console.log(event.port)
     }
 
     render() {
         return html`
             <h1>Midi</h1>
-
             <wa-button @click=${this.initSynth}>Init synth</wa-button>
+
+            ${this.midiConnected ? html`
+                <p>Connected devices ${this.midiDevicesConnected}` : nothing
+            }
         `
     }
 }
--- client/src/design/waLoader.ts
+++ client/src/design/waLoader.ts
@@ -1,9 +1,40 @@
-import '@awesome.me/webawesome/dist/styles/webawesome.css';
-import '@awesome.me/webawesome/dist/components/button/button.js';
-import '@awesome.me/webawesome/dist/components/input/input.js';
-import "@awesome.me/webawesome/dist/components/avatar/avatar.js"
-import "@awesome.me/webawesome/dist/components/icon/icon.js"
-import '@awesome.me/webawesome/dist/components/slider/slider.js';
-import '@awesome.me/webawesome/dist/components/checkbox/checkbox.js';
-import '@awesome.me/webawesome/dist/components/badge/badge.js';
-import '@awesome.me/webawesome/dist/components/breadcrumb/breadcrumb.js';
+
+const modulesToLoad = [
+    "/components/button/button.js",      // wa-button
+    "/components/icon/icon.js",          // wa-icon
+    "/components/tooltip/tooltip.js",    // wa-tooltip
+    "/components/checkbox/checkbox.js",   // wa-checkbox
+    "/components/popover/popover.js",    // wa-popover
+    "/components/avatar/avatar.js",      // wa-avatar
+    "/components/input/input.js",        // wa-input
+    "/components/dialog/dialog.js",      // wa-dialog 
+    "/components/callout/callout.js",     // wa-callout
+    "/components/slider/slider.js",
+    "/components/badge/badge.js",
+    "/components/breadcrumb/breadcrumb.js"
+]
+
+const baseUrl = "https://static.logout.work/webawesome/3.1.0/dist"
+
+const mainCss = "/styles/themes/default.css"
+
+function createCss() {
+    const styleCss = document.createElement("link")
+    styleCss.rel = "stylesheet"
+    styleCss.href = `${baseUrl}${mainCss}`
+    document.head.appendChild(styleCss)
+}
+
+async function importComponents() {
+    createCss()
+
+    const promises = modulesToLoad.map(m => import(`${baseUrl}${m}`))
+
+    await Promise.all(promises)
+    return true
+}
+
+export async function doImportWa() {
+    const success = await importComponents();
+    return success
+}
